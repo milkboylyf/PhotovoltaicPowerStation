b35eac4833b02f7ab1d946b723e5baf393d2833b
milkboylyf <liyifan0075@163.com>
2018-09-25 23:59:37 +0800

    the code is learing from other code
    the detail description can been seen in https://zhuanlan.zhihu.com/p/44755488?utm_source=wechat_session&utm_medium=social&utm_oi=623925402599559168&from=singlemessage&isappinstalled=0&wechatShare=1

---
+++ main.py
@@ -0,0 +1,17 @@
+# numpy and pandas for data manipulation
+import numpy as np
+import pandas as pd
+# sklearn preprocessing for dealing with categorical variables
+from sklearn.preprocessing import LabelEncoder
+# File system manangement
+import os
+# Suppress warnings
+import warnings
+warnings.filterwarnings('ignore')
+# matplotlib and seaborn for plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+import lightgbm as lgb
+from sklearn import metrics
+
+
